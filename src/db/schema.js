7b09717dc0538a94249e1eeca9b130004ede8784
moebrainz <moebrainz@gmail.com>
2026-01-20 17:07:55 +0100

    changes in server, recipe

--- src/db/schema.js
+++ src/db/schema.js
@@ -3,7 +3,7 @@ import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 export const favouritesTable = pgTable("favourites", {
   id: serial("id").primaryKey(),
   userId: text("user_id", { length: 255 }).notNull(),
-  recipieId: integer("recipie_id", { length: 255 }).notNull(),
+  recipeId: integer("recipie_id", { length: 255 }).notNull(),
   title: text("title", { length: 255 }).notNull(),
   image: text("image"),
   cookTime: text("cook_time"),
